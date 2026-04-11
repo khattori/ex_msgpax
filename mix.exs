84cf8dba952c63db0b66e315f6c31df6f768b700
Kenta HATTORI <kenta_hattori@hotmail.com>
2026-04-11 10:37:14 +0900

    Updated mix.exs

--- mix.exs
+++ mix.exs
@@ -1,14 +1,22 @@
 defmodule ExMsgpax.MixProject do
   use Mix.Project
 
+  @description """
+  ExMsgpax is a library for packing and unpacking Elixir data structures using MessagePack format.
+  It provides support for various Elixir types, including tuples, structs, and exceptions,
+  by utilizing MessagePack's extension types.
+  """
   def project do
     [
       app: :ex_msgpax,
+      description: @description,
+      name: "ExMsgpax",
       version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
+      package: package(),
       versioning: versioning()
     ]
   end
@@ -31,6 +39,15 @@ defmodule ExMsgpax.MixProject do
       {:mix_version, "~> 2.4", only: [:dev, :test], runtime: false},
     ]
   end
+
+  defp package do
+    [
+      maintainers: ["hattori"],
+      licenses: ["MIT"],
+      links: %{ "Github" => "https://github.com/khattori/ex_msgpax" }
+    ]
+  end
+
 
   defp versioning do
     [
